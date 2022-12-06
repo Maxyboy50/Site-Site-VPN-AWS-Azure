f3c7b2c861e293e5a1832790bb20d1ea66962e3d
root <root@DESKTOP-HD9RTP5.localdomain>
2022-12-05 20:49:59 -0600

    Removing value for .gitignore

--- aws_variables.tf
+++ aws_variables.tf
@@ -1,5 +1,5 @@
 variable "aws_customer_gateway_name" {
-  description = "Name to assign the Customer Gateway Device (e.g. Client-1-Customer-Gateway"
+  description = "Name to assign the Customer Gateway Device (e.g. Client-1-Customer-Gateway)"
   type        = string
 }
 
@@ -17,24 +17,24 @@ variable "type_customer_gateway" {
 
 ##################VPN Connection Variables#######################
 variable "local_network_cidr" {
-  description = "CIDR block of local network that is allowed to communicate through VPN"
+  description = "CIDR block belonging to local network that is allowed to communicate through VPN"
   type        = string
 }
 
 variable "aws_side_cidr" {
-  description = "CIDR block of aws resources that are allowed to communicate through VPN"
+  description = "CIDR block of AWS resources that are allowed to communicate through VPN"
   type        = string
 }
 
 variable "type_vpn_connection" {
-  description = "VPN connection type for site-site resource"
+  description = "VPN connection type for VPN connection resource"
   type        = string
   default     = "ipsec.1"
 }
 
 #####################Subnet Variables#######################
 variable "cidr_block_aws" {
-  description = "CIDR notation for subnet"
+  description = "CIDR notation for subnet (must fall within VPC CIDR block, e.g. 10.0.1.0/24)"
   type        = string
 
 }
@@ -51,5 +51,5 @@ variable "tags" {
 #####################Virtual Private Cloud Variables##########################
 variable "vpc_cidr_block" {
   type        = string
-  description = "CIDR notation block to use for VPC"
+  description = "CIDR notation block to use for VPC (e.g. 10.0.0.0/16)"
 }
